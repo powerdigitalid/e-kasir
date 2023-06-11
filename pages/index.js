bf188ca3f1f29629cb5a2d0fdb94a02557500eee
SetiyoPramono <setiyopramono04@gmail.com>
2023-06-11 22:48:33 +0700

    firt commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-no-target-blank */
+import Login from "components/auth/login";
+import React from "react";
+
+
+export default function Index() {
+  return (
+    <>
+     <Login />
+    </>
+  );
+}
---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import Register from 'components/auth/register'
+import Auth from 'layouts/Auth'
+import React from 'react'
+
+export default function register() {
+  return (
+    <>
+    <Auth>
+        <Register />
+    </Auth>
+    </>
+  )
+}
