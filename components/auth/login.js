021c6390d37970006f601e0dc73ec312c61256b8
veralindha <fangkyistiawan@gmail.com>
2023-06-26 15:37:58 +0700

    change link fetch

--- components/auth/login.js
+++ components/auth/login.js
@@ -1,63 +1,68 @@
-import React, {useState} from 'react';
-import {useRouter} from 'next/router';
-import {setCookie} from '../../libs/cookie.lib';
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { setCookie } from "../../libs/cookie.lib";
 // import Link from "next/link";
 
 // layout for page
 import Auth from "layouts/Auth";
 
-
 export default function Login() {
-    const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState("")
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const router = useRouter();
 
-  const handleLogin = (e) =>{
+  const handleLogin = (e) => {
     e.preventDefault();
     setLoading(true);
-    fetch('api/auth/login', {
+    fetch("https://e-kasir.vercel.app/api/auth/login", {
       method: "POST",
-      headers:{
-        "Content-Type": "application/json"
+      headers: {
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         username,
-        password
+        password,
       }),
     })
-    .then((res) => res.json())
-    .then((data) => {
-      if(data.message === "Login success" && data.user.token){
-        alert("Login Success!", "Login Success Redirected in 3 second!", "success");
-        setCookie("token", data.user.token, 1);
-        router.push("/admin/dashboard");
-      } else {
-        setError(data.message);
-      }
-      setLoading(false);
-    })
-    .catch((err) => {
-      setError(err);
-      alert("Login Failed!", "Login Failed Redirected in 3 secon!" + err, "error");
-      setLoading(false)
-    });
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message === "Login success" && data.user.token) {
+          alert(
+            "Login Success!",
+            "Login Success Redirected in 3 second!",
+            "success"
+          );
+          setCookie("token", data.user.token, 1);
+          router.push("/admin/dashboard");
+        } else {
+          setError(data.message);
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        alert(
+          "Login Failed!",
+          "Login Failed Redirected in 3 secon!" + err,
+          "error"
+        );
+        setLoading(false);
+      });
   };
   return (
     <>
-    <Auth>
-      <div className="container mx-auto px-4 h-full">
-        <div className="flex content-center items-center justify-center h-full">
-          <div className="w-full lg:w-4/12 px-4">
-            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-              <div className="rounded-t mb-0 px-6 py-6">
-                <div className="text-center mb-3">
-                  <h2 className="text-dark-500 font-bold">
-                    LOGIN
-                  </h2>
-                </div>
-                {/* <div className="btn-wrapper text-center">
+      <Auth>
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex content-center items-center justify-center h-full">
+            <div className="w-full lg:w-4/12 px-4">
+              <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+                <div className="rounded-t mb-0 px-6 py-6">
+                  <div className="text-center mb-3">
+                    <h2 className="text-dark-500 font-bold">LOGIN</h2>
+                  </div>
+                  {/* <div className="btn-wrapper text-center">
                   <button
                     className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
                     type="button"
@@ -73,43 +78,45 @@ export default function Login() {
                     Google
                   </button>
                 </div> */}
-                <hr className="mt-6 border-b-1 border-blueGray-300" />
-              </div>
-              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                {/* <div className="text-blueGray-400 text-center mb-3 font-bold">
+                  <hr className="mt-6 border-b-1 border-blueGray-300" />
+                </div>
+                <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                  {/* <div className="text-blueGray-400 text-center mb-3 font-bold">
                   <small>Or sign in with credentials</small>
                 </div> */}
-                <form>
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Username
-                    </label>
-                    <input
-                      type="text"
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Username"
-                      value={username} onChange={(e)=> setUsername(e.target.value)}
-                    />
-                  </div>
+                  <form>
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        Username
+                      </label>
+                      <input
+                        type="text"
+                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                      />
+                    </div>
 
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Password"
-                      value={password} onChange={(e)=> setPassword(e.target.value)}
-                    />
-                  </div>
-                  {/* <div>
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
+                    {/* <div>
                     <label className="inline-flex items-center cursor-pointer">
                       <input
                         id="customCheckLogin"
@@ -122,19 +129,19 @@ export default function Login() {
                     </label>
                   </div> */}
 
-                  <div className="text-center mt-6">
-                    <button
-                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                      type="submit"
-                      onClick={handleLogin}
-                    >
-                      LOGIN
-                    </button>
-                  </div>
-                </form>
+                    <div className="text-center mt-6">
+                      <button
+                        className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                        type="submit"
+                        onClick={handleLogin}
+                      >
+                        LOGIN
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
-            </div>
-            {/* <div className="flex flex-wrap mt-6 relative">
+              {/* <div className="flex flex-wrap mt-6 relative">
               <div className="w-1/2">
                 <a
                   href="#pablo"
@@ -150,10 +157,10 @@ export default function Login() {
                 </Link>
               </div>
             </div> */}
+            </div>
           </div>
         </div>
-      </div>
-    </Auth>
+      </Auth>
     </>
   );
 }
