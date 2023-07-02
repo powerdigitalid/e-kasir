3232751874f70293f256e4c0a023734c2cf5b5b2
AlvifSandana <asm240800@gmail.com>
2023-07-02 23:40:23 +0700

    [alvif]: next-auth fixing

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,56 +1,35 @@
 import NextAuth from "next-auth";
-import { Providers } from "next-auth/providers/google";
-import { Google } from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
-const validateCredentials = async (username, password) => {
-  // Implement your own logic to validate the credentials
-  // For example, check if the username and password match a user in your database
-  const validUsername = "kimeee220801@gmail.com";
-  const validPassword = "password123";
-
-  return username === validUsername && password === validPassword;
-};
+const validEmail = process.env.GOOGLE_USER;
+const devUser = process.env.DEV_USER; // Email yang diizinkan untuk login
 
 export default NextAuth({
   providers: [
-    Providers.Credentials({
-      name: "Email and Password",
-      credentials: {
-        username: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      authorize: async (credentials) => {
-        const { username, password } = credentials;
-
-        if (username.endsWith("@gmail.com")) {
-          // Only allow login with Google email
-          const isValid = await validateCredentials(username, password);
-
-          if (isValid) {
-            return Promise.resolve({ email: username });
-          } else {
-            throw new Error("Invalid credentials");
-          }
-        } else {
-          throw new Error("Invalid email domain");
-        }
-      },
-    }),
-    Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      // Memeriksa apakah email yang masuk sesuai dengan email yang diizinkan
+      const email = user.profile?.email || user.profile?.emails?.[0]?.value;
+      if (email === validEmail || email === devUser) {
+        return Promise.resolve(true);
+      } else {
+        return Promise.resolve(false);
+      }
+    },
+    async redirect(url, baseUrl) {
+      return Promise.resolve("/admin/dashboard");
+    },
+  },
   session: {
     jwt: true,
-    maxAge: 60 * 60, // 1 hour
+    maxAge: 60 * 60, // 1 jam
   },
   jwt: {
     secret: process.env.JWT_SECRET,
-  },
-  callbacks: {
-    redirect: async (url, baseUrl) => {
-      return Promise.resolve(baseUrl);
-    },
   },
 });
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,8 @@
 import React from "react";
-import { useState } from "react";
 import { signIn } from "next-auth/react";
 import Auth from "layouts/Auth";
-import {useRouter } from 'next/router'
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    alert("login Success")
-    signIn("credentials", {
-      email,
-      password,
-      callbackUrl: `${window.location.origin}/dashboard`,
-    });
-    
-  };
-
   const handleGoogleLogin = (e) => {
     e.preventDefault();
     signIn("google", { callbackUrl: `${window.location.origin}/dashboard` });
@@ -34,71 +10,27 @@ export default function LoginPage() {
 
   return (
     <Auth>
-    <div>
-      <div className="container mx-auto px-4 h-full">
-        <div className="flex content-center items-center justify-center h-full">
-          <div className="w-full lg:w-4/12 px-4">
-            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-              <div className="rounded-t mb-0 px-6 py-6">
-                <div className="text-center mb-3">
-                  <h2 className="text-dark-500 font-bold">LOGIN</h2>
+      <div>
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex content-center items-center justify-center h-full">
+            <div className="w-full lg:w-4/12 px-4">
+              <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+                <div className="rounded-t mb-0 px-6 py-6">
+                  <div className="text-center mb-3">
+                    <h2 className="text-dark-500 font-bold">LOGIN</h2>
+                  </div>
+                  <hr className="mt-6 border-b-1 border-blueGray-300" />
                 </div>
-                <hr className="mt-6 border-b-1 border-blueGray-300" />
-              </div>
-              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                <form onSubmit={handleFormSubmit}>
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Email"
-                      name="email"
-                      value={email}
-            onChange={handleEmailChange}
-                    />
-                  </div>
-
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Password"
-                      name="password"
-                      value={password}
-            onChange={handlePasswordChange}
-                    />
-                  </div>
-
-                  <div className="text-center mt-6">
-                    <button
-                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                      type="submit"
-                    >
-                      LOGIN
-                    </button>
-                  </div>
-                </form>
-                <button onClick={handleGoogleLogin} className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mt-3 mb-1 w-full ease-linear transition-all duration-150">
-                  LOGIN WITH GOOGLE
-                </button>
+                <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                  <button onClick={handleGoogleLogin} className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mt-3 mb-1 w-full ease-linear transition-all duration-150">
+                    LOGIN WITH GOOGLE
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </Auth>
   );
 }
